195c8c205df07fff4e986f16aa60646051d06efc
shruti-coder22 <79634442+shruti-coder22@users.noreply.github.com>
2021-06-06 10:05:21 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+menu_list_array = ["mahgerita pizza", 
+                    "chikaroni pizza", 
+                    "pepparonni pizza"] ;
+
+function getmenu() {
+   var htmldata; 
+   htmldata = "<ol class = 'menulist'";
+   menu_list_array.sort();
+
+   for (var i=0; i<menu_list_array.length(); i++) {
+       htmldata = htmldata + "<li>" + menu_list_array[i] + "</li>";
+   }
+
+   htmldata = htmldata + "</ol>";
+   document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function additem() {
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata = "<section class = 'card'>" + "<img source = 'pizzaImg.png'>" + menu_list_array[i] + "</div>";
+    htmldata = htmldata + "</section>";
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
